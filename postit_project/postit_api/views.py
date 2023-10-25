54a0d7821123175b9cd1bd4307c4c6cab3940af8
juliusbutkus <j.butkus.skouds@gmail.com>
2023-10-25 09:52:26 +0300

    netsimenu statuso

--- postit_project/postit_api/views.py
+++ postit_project/postit_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import models, serializers
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+
